2b691c112ce6ade143ada9840d969abf373b3e39
RITIKA MOLPARIYA <46062915+rit9163882@users.noreply.github.com>
2019-04-06 22:46:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -28,12 +28,12 @@ Vue.component('modal',{
           <div class="modal-background"></div>
           <div class="modal-content">
             <div class="box"> <div id="conclave.register">
-            <p style="text-align:center; justify-content: center">Participate in the Case Study Challenge, Kartavya 2019</p>
-        <button class="button-reg"><a href="https://forms.gle/GMSCpLhrGKXEx8Sy7" style="color: #FFFFFF"><span>Register here..</span></button>
+            <p style="text-align:center; justify-content: center">Participate in the Case Study Challenge, Kartavya 2019
+  <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button></p>
+        <button class="button-reg"><a href="https://forms.gle/GMSCpLhrGKXEx8Sy7" style="color: #FFFFFF"><span>Register here..</span></a></button>
         </div></div>
            
-          </div>
-  <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+          </div> 
   </div>
   `
 });     
