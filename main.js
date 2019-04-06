803768930f8bfa0ad2bfd972010bd8f709cfd3c4
RITIKA MOLPARIYA <46062915+rit9163882@users.noreply.github.com>
2019-04-06 22:15:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+    // Vue.component('message',{
+    //     props:['title','body'],
+    //     data(){
+    //         return {
+    //             isvisible:true
+    //         };
+    //     },
+    //     template:`
+    //     <article class="message"v-show="isvisible">
+    //         <div class="message-header">
+    //         {{title}}
+    //         <button class="button" @click="hidemessage" >x</button>
+    //         </div>
+
+    //         <div class="message-body">
+    //         {{body}}
+    //         </div>
+    //     </article>`,
+    //     methods:{
+    //         hidemessage:function(){
+    //             this.isvisible=false;
+    //         }
+    //     }
+    // });
+Vue.component('modal',{
+    template:`
+    <div class="modal is-active" >
+          <div class="modal-background"></div>
+          <div class="modal-content">
+            <div class="box"> <div id="conclave.register">
+            <p style="text-align:center; justify-content: center">Participate in the Case Study Challenge, Kartavya 2019</p>
+        <button class="button-reg"><a href="https://forms.gle/GMSCpLhrGKXEx8Sy7" style="color: #FFFFFF"><span>Register here..</span></button>
+        </div></div>
+           
+          </div>
+  <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+  </div>
+  `
+});     
+new Vue({
+    el: '#root',
+    data:{
+        showModal:false
+    }
+    
+});
